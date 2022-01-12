c1e10fed127b12264c700890b555d571dd3b3085
zhout <zhout@wyn88.com>
2022-01-12 10:59:03 +0800

    perf: 整合目录

--- core/cli/bin/index.js
+++ core/cli/bin/index.js
@@ -4,7 +4,7 @@ const importLocal = require('import-local')
 const npmLog = require('npmlog')
 
 if (importLocal(__filename)) {
-    npmLog.info('cli', '正在使用robotdm-cli本地版本')
+    npmLog.info('cli', '正在使用robotdm-cli-dev本地版本')
 } else {
     require('../lib')(process.argv.slice(2))
 }
